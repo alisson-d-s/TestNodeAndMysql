05afd7d16c7fd48348e13cdcf697a0a7a7d2bc84
TI-N001 <alisson_d_s@live.com>
2022-03-22 11:12:50 -0300

    Created config to hide password

--- src/database/dbMysql.js
+++ src/database/dbMysql.js
@@ -1,8 +1,10 @@
+const dbPass = require('../config/dbPass.json');
+
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: '192.168.1.192',
     user: 'root',
-    password: '758hHuB153*',
+    password: dbPass.pass,
     database: 'RobotBet'
 });
 
